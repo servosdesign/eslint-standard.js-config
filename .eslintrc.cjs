36ba9c9763ef081ab5c3df19284e00f7d673f681
servosdesign <servos.design0@gmail.com>
2023-05-18 14:09:01 -0500

    .eslint.cjs and package.json

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
